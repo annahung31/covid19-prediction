4fea80bfba791572ef8e2ca07373a8a3ec9de967
annahung31 <fbiannahung@gmail.com>
2020-06-12 12:23:40 +0800

    init

---
+++ demo/model_src/models/COVIDNet-CXR3-C/download_pretrain.sh
@@ -0,0 +1,8 @@
+
+alias gdrive.sh='curl gdrive.sh | bash -s'
+echo "start downloading..."
+gdrive.sh 1elCyfrkMI78Xdrm1XBJJoQdL5erOGTgS
+gdrive.sh 1aQhNCnINJjnJDvCw7Igb38l7Asv8rMWi
+gdrive.sh 1kj7xvn1EQou7WvNlU20wJUVlnbrJSx6Q
+gdrive.sh 1QOJLXV63XpBYw29sQF7ymUrMhaw3AOpr
+echo "all done."
